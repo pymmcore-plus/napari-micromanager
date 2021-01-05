5ec043c5aa15ff2fca5ab532f37c491d87ed9a27
fdrgsp <70725613+fdrgsp@users.noreply.github.com>
2021-01-05 16:33:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="micromanager",
+    version="0.0.1",
+    description="A micromanager gui in python.",
+    author="Federico Gasparoli",
+    author_email="federico.gasparoli@gmail.com",
+    url="https://github.com/fdrgsp/micromanager.git",
+    packages=find_packages(),
+    install_requires=[
+        'napari[all]',
+        'numpy',
+        'scikit-image',
+        'matplotlib'
+        ]
+)
