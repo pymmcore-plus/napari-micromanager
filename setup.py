7e935749ff3dc0cc1e80cce6647c772e6d4867ce
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2021-01-22 09:46:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="micromanager",
+    name="napari-micromanager",
     version="0.0.1",
     description="A python gui for micro-manager",
     author="Federico Gasparoli",
