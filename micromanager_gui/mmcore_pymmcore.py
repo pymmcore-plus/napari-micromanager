194c68a5209d4decd16566acd437c169fe846be2
fdrgsp <federico.gasparoli@gmail.com>
2021-01-28 10:19:34 -0500

    update mmcore_pymmcore

--- micromanager_gui/mmcore_pymmcore.py
+++ micromanager_gui/mmcore_pymmcore.py
@@ -22,98 +22,89 @@ def find_micromanager():
     except StopIteration:
         print("could not find micromanager directory")
 
+class MMCore(QObject):
+    properties_changed = Signal()
+    property_changed = Signal(str, str, object)
+    channel_group_changed = Signal(str)
+    config_group_changed = Signal(str, str)
+    system_configuration_loaded = Signal()
+    pixel_size_changed = Signal(float)
+    pixel_size_affine_changed = Signal(float, float, float, float, float, float)
+    stage_position_changed = Signal(str, float)
+    xy_stage_position_changed = Signal(str, float, float)
+    exposure_changed = Signal(str, float)
+    slm_exposure_changed = Signal(str, float)
 
+    __instance = None
 
-class MMCore:
-    _instance = None
     # Singleton pattern: https://python-patterns.guide/gang-of-four/singleton/
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = pymmcore.CMMCore(*args, **kwargs)# = to: mmcore = pymmcore.CMMCore()
-            cls._instance.setDeviceAdapterSearchPaths([find_micromanager()])# = to: mmcore.setDeviceAdapterSearchPaths([])
-        return cls._instance
+    def __new__(cls) -> pymmcore.CMMCore:
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance._initialized = False
+        return cls.__instance
+
+    def __init__(self, adapter_paths=None):
+        if self._initialized:
+            return
+        super().__init__()
+        self._mmc = pymmcore.CMMCore()
+        if not adapter_paths:
+            adapter_paths = [find_micromanager()]
+            print(f'Micromanager path: {adapter_paths}')
+        self._mmc.setDeviceAdapterSearchPaths(adapter_paths)
+        self._callback = CallbackRelay(self)
+        self._mmc.registerCallback(self._callback)
+        self._initialized = True
+
+    def __getattr__(self, name):
+        return getattr(self._mmc, name)
+
+    def __dir__(self):
+        return set(object.__dir__(self)).union(dir(self._mmc))
+
+    @property
+    def setProperty(self):
+        # conflicts with QObject.setProperty
+        return self._mmc.setProperty
 
 
-# class MMCore(QObject):
-#     properties_changed = Signal()
-#     property_changed = Signal(str, str, object)
-#     channel_group_changed = Signal(str)
-#     config_group_changed = Signal(str, str)
-#     system_configuration_loaded = Signal()
-#     pixel_size_changed = Signal(float)
-#     pixel_size_affine_changed = Signal(float, float, float, float, float, float)
-#     stage_position_changed = Signal(str, float)
-#     xy_stage_position_changed = Signal(str, float, float)
-#     exposure_changed = Signal(str, float)
-#     slm_exposure_changed = Signal(str, float)
+class CallbackRelay(pymmcore.MMEventCallback):
+    def __init__(self, emitter):
+        super().__init__()
+        self._emitter = emitter
 
-#     __instance = None
+    def onPropertiesChanged(self):
+        self._emitter.properties_changed.emit()
 
-#     # Singleton pattern: https://python-patterns.guide/gang-of-four/singleton/
-#     def __new__(cls) -> pymmcore.CMMCore:
-#         if cls.__instance is None:
-#             cls.__instance = super().__new__(cls)
-#             cls.__instance._initialized = False
-#         return cls.__instance
+    def onPropertyChanged(self, dev_name: str, prop_name: str, prop_val: str):
+        self._emitter.property_changed.emit(dev_name, prop_name, prop_val)
 
-#     def __init__(self, adapter_paths=None):
-#         if self._initialized:
-#             return
-#         super().__init__()
-#         self._mmc = pymmcore.CMMCore()
-#         if not adapter_paths:
-#             adapter_paths = [find_micromanager()]
-#         self._mmc.setDeviceAdapterSearchPaths(adapter_paths)
-#         self._callback = CallbackRelay(self)
-#         self._mmc.registerCallback(self._callback)
-#         self._initialized = True
+    def onChannelGroupChanged(self, new_channel_group_name: str):
+        self._emitter.channel_group_changed.emit(new_channel_group_name)
 
-#     def __getattr__(self, name):
-#         return getattr(self._mmc, name)
+    def onConfigGroupChanged(self, group_name: str, new_config_name: str):
+        self._emitter.config_group_changed.emit(group_name, new_config_name)
 
-#     def __dir__(self):
-#         return set(object.__dir__(self)).union(dir(self._mmc))
+    def onSystemConfigurationLoaded(self):
+        self._emitter.system_configuration_loaded.emit()
 
-#     @property
-#     def setProperty(self):
-#         # conflicts with QObject.setProperty
-#         return self._mmc.setProperty
+    def onPixelSizeChanged(self, new_pixel_size_um: float):
+        self._emitter.pixel_size_changed.emit(new_pixel_size_um)
 
+    def onPixelSizeAffineChanged(self, v0, v1, v2, v3, v4, v5):
+        self._emitter.pixel_size_affine_changed.emit(v0, v1, v2, v3, v4, v5)
 
-# class CallbackRelay(pymmcore.MMEventCallback):
-#     def __init__(self, emitter):
-#         super().__init__()
-#         self._emitter = emitter
+    def onStagePositionChanged(self, name: str, pos: float):
+        self._emitter.stage_position_changed.emit(name, pos)
 
-#     def onPropertiesChanged(self):
-#         self._emitter.properties_changed.emit()
+    def onXYStagePositionChanged(self, name: str, xpos: float, ypos: float):
+        self._emitter.xy_stage_position_changed.emit(name, xpos, ypos)
 
-#     def onPropertyChanged(self, dev_name: str, prop_name: str, prop_val: str):
-#         self._emitter.property_changed.emit(dev_name, prop_name, prop_val)
+    def onExposureChanged(self, name: str, new_exposure: float):
+        self._emitter.exposure_changed.emit(name, new_exposure)
 
-#     def onChannelGroupChanged(self, new_channel_group_name: str):
-#         self._emitter.channel_group_changed.emit(new_channel_group_name)
+    def onSLMExposureChanged(self, name: str, new_exposure: float):
+        self._emitter.slm_exposure_changed.emit(name, new_exposure)
+        
 
-#     def onConfigGroupChanged(self, group_name: str, new_config_name: str):
-#         self._emitter.config_group_changed.emit(group_name, new_config_name)
-
-#     def onSystemConfigurationLoaded(self):
-#         self._emitter.system_configuration_loaded.emit()
-
-#     def onPixelSizeChanged(self, new_pixel_size_um: float):
-#         self._emitter.pixel_size_changed.emit(new_pixel_size_um)
-
-#     def onPixelSizeAffineChanged(self, v0, v1, v2, v3, v4, v5):
-#         self._emitter.pixel_size_affine_changed.emit(v0, v1, v2, v3, v4, v5)
-
-#     def onStagePositionChanged(self, name: str, pos: float):
-#         self._emitter.stage_position_changed.emit(name, pos)
-
-#     def onXYStagePositionChanged(self, name: str, xpos: float, ypos: float):
-#         self._emitter.xy_stage_position_changed.emit(name, xpos, ypos)
-
-#     def onExposureChanged(self, name: str, new_exposure: float):
-#         self._emitter.exposure_changed.emit(name, new_exposure)
-
-#     def onSLMExposureChanged(self, name: str, new_exposure: float):
-#         self._emitter.slm_exposure_changed.emit(name, new_exposure)
