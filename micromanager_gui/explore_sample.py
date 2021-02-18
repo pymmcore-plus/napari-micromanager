e6158fc6b97eb49b2a7c4d490526c8d469305292
fdrgsp <federico.gasparoli@gmail.com>
2021-02-18 10:19:30 -0500

    fix Row x Col explore

--- micromanager_gui/explore_sample.py
+++ micromanager_gui/explore_sample.py
@@ -21,8 +21,8 @@ mmcore = MMCore()
 class ExploreSample(QtW.QWidget):
     # The UI_FILE above contains these objects:
     scan_size_label: QtW.QLabel
-    scan_size_spinBox_x: QtW.QSpinBox
-    scan_size_spinBox_y: QtW.QSpinBox
+    scan_size_spinBox_r: QtW.QSpinBox
+    scan_size_spinBox_c: QtW.QSpinBox
     scan_channel_comboBox: QtW.QComboBox
     scan_exp_spinBox: QtW.QSpinBox
     start_scan_Button: QtW.QPushButton
@@ -48,11 +48,9 @@ class ExploreSample(QtW.QWidget):
         self.stitched_image_array_list = []
         self.x_lineEdit.setText('None')
         self.y_lineEdit.setText('None')
-        # self.scan_size = 'None'
-        self.scan_size_x = 'None'
-        self.scan_size_y = 'None'
+        self.scan_size_r = 'None'
+        self.scan_size_c = 'None'
 
-       
         self.start_scan_Button.clicked.connect(self.start_scan)
         self.stop_scan_Button.clicked.connect(self.stop_scan)
         self.move_to_Button.clicked.connect(self.move_to)
@@ -62,34 +60,27 @@ class ExploreSample(QtW.QWidget):
 
     def start_scan(self):
 
-        # name = f'stitched_{self.scan_size}x{self.scan_size}'
-        name = f'stitched_{self.scan_size_x}x{self.scan_size_y}'
+        name = f'stitched_{self.scan_size_r}x{self.scan_size_c}'
         self.delete_previous_scan.emit(name)#emot signal to MainWindow
 
         self.x_lineEdit.setText('None')
         self.y_lineEdit.setText('None')
 
-        # self.scan_size = self.scan_size_spinBox.value()
-        self.scan_size_x = self.scan_size_spinBox_x.value()#self.scan_size_spinBox_x
-        self.scan_size_y = self.scan_size_spinBox_y.value()#self.scan_size_spinBox_y
+        self.scan_size_r = self.scan_size_spinBox_r.value()
+        self.scan_size_c = self.scan_size_spinBox_c.value()
 
         self.scan_position_list.clear()
         self.stitched_image_array_list.clear()
         
         self.scaling_factor = 3
 
-        # self.total_size = self.scan_size*self.scan_size
-        self.total_size = self.scan_size_x*self.scan_size_y
+        self.total_size = self.scan_size_r*self.scan_size_c
 
         #create positions storage arrays
-        # self.array_pos_x = np.empty((self.scan_size,self.scan_size))
-        # self.array_pos_y  = np.empty((self.scan_size,self.scan_size))
-        # self.array_pos_z  = np.empty((self.scan_size,self.scan_size))
-        self.array_pos_x = np.empty((self.scan_size_x,self.scan_size_y))
-        self.array_pos_y  = np.empty((self.scan_size_x,self.scan_size_y))
-        self.array_pos_z  = np.empty((self.scan_size_x,self.scan_size_y))
+        self.array_pos_x = np.empty((self.scan_size_r,self.scan_size_c))
+        self.array_pos_y  = np.empty((self.scan_size_r,self.scan_size_c))
+        self.array_pos_z  = np.empty((self.scan_size_r,self.scan_size_c))
         
-
         #for progress bar
         prigress_values = np.linspace(1, 90, self.total_size)
         prigress_values = np.round(prigress_values,0)
@@ -108,11 +99,8 @@ class ExploreSample(QtW.QWidget):
         #calculate initial scan position
         self.width = mmcore.getROI(mmcore.getCameraDevice())[2]#maybe they are inverted
         self.height = mmcore.getROI(mmcore.getCameraDevice())[3]#maybe they are inverted
-        # move_x = (self.width/2)*(self.scan_size-1)*mmcore.getPixelSizeUm()
-        # move_y = (self.height/2)*(self.scan_size-1)*mmcore.getPixelSizeUm()
-        move_x = (self.width/2)*(self.scan_size_x-1)*mmcore.getPixelSizeUm()
-        move_y = (self.height/2)*(self.scan_size_y-1)*mmcore.getPixelSizeUm()
-
+        move_x = (self.width/2)*(self.scan_size_r-1)*mmcore.getPixelSizeUm()
+        move_y = (self.height/2)*(self.scan_size_c-1)*mmcore.getPixelSizeUm()
 
         x_pos_explorer = x_curr_pos_explorer - move_x
         y_pos_explorer = y_curr_pos_explorer + move_y
@@ -124,24 +112,19 @@ class ExploreSample(QtW.QWidget):
         # print(f'increments: {increment_x},{increment_y}')
 
         #create the xyz position matrix 
-        # for r in range(self.scan_size):
-        for r in range(self.scan_size_x):
+        for r in range(self.scan_size_r):
             if r == 0 or (r % 2) == 0:
-                # for c in range(self.scan_size):#for even rows
-                for c in range(self.scan_size_y):
+                for c in range(self.scan_size_c):#for even rows
                     if r>0 and c == 0:
                         y_pos_explorer = y_pos_explorer - increment_y
                     self.array_pos_x[r][c] = x_pos_explorer
                     self.array_pos_y[r][c] = y_pos_explorer
                     self.array_pos_z[r][c] = z_curr_pos_explorer
-                    # if c < self.scan_size-1:
-                    if c < self.scan_size_y-1:
+                    if c < self.scan_size_c-1:
                         x_pos_explorer = x_pos_explorer + increment_x   
             else:#for odd rows
-                # col = self.scan_size-1
-                col = self.scan_size_y-1
-                # for c in range(self.scan_size):
-                for c in range(self.scan_size_y):
+                col = self.scan_size_c-1
+                for c in range(self.scan_size_c):
                     if c == 0:
                         y_pos_explorer = y_pos_explorer - increment_y
                     self.array_pos_x[r][col] = x_pos_explorer
@@ -155,12 +138,10 @@ class ExploreSample(QtW.QWidget):
 
         #move to the correct position and acquire an image
         progress = 0
-        # for row in range(self.scan_size):
-        for row in range(self.scan_size_x):
+        for row in range(self.scan_size_r):
             if row == 0 or (row % 2) == 0:#for even rows
                 # print(f'row {row} is even')
-                # for s in range(self.scan_size):
-                for s in range(self.scan_size_y):      
+                for s in range(self.scan_size_c):      
                     # move to position
                     vx = self.array_pos_x[row][s]
                     vy = self.array_pos_y[row][s]
@@ -197,10 +178,8 @@ class ExploreSample(QtW.QWidget):
 
             else:#for odd rows
                 # print(f'row {row} is odd')
-                # col = self.scan_size-1
-                col = self.scan_size_y-1
-                # for s in range(self.scan_size):
-                for s in range(self.scan_size_y):
+                col = self.scan_size_c-1
+                for s in range(self.scan_size_c):
                     # print(f'col = {col}')
 
                     # move to position
@@ -244,9 +223,8 @@ class ExploreSample(QtW.QWidget):
         for row in range(1,len(self.stitched_image_array_list)):
             st = self.stitched_image_array_list[row]
             stitched_image_final = np.concatenate((stitched_image_final, st), axis = 0)
-        # print(f'stitched_image_final.shape = {stitched_image_final.shape}')
-        # self.new_frame.emit(f'stitched_{self.scan_size}x{self.scan_size}', stitched_image_final)
-        self.new_frame.emit(f'stitched_{self.scan_size_x}x{self.scan_size_y}', stitched_image_final)
+        print(f'stitched_image_final.shape = {stitched_image_final.shape}')
+        self.new_frame.emit(f'stitched_{self.scan_size_r}x{self.scan_size_c}', stitched_image_final)
         self.progressBar.setValue(100)
         self.shape_stitched_x = stitched_image_final.shape[1]
         self.shape_stitched_y = stitched_image_final.shape[0]
@@ -265,10 +243,8 @@ class ExploreSample(QtW.QWidget):
             coord_y = float(string_coord_y)
             # print(f'COORDS: {coord_x},{coord_y}')
 
-            # x_snap = self.shape_stitched_x/self.scan_size
-            # y_snap = self.shape_stitched_y/self.scan_size
-            x_snap = self.shape_stitched_x/self.scan_size_x
-            y_snap = self.shape_stitched_y/self.scan_size_y
+            x_snap = self.shape_stitched_x/self.scan_size_c
+            y_snap = self.shape_stitched_y/self.scan_size_r
             # print(x_snap, y_snap)
 
             x_snap_increment = x_snap
@@ -283,10 +259,10 @@ class ExploreSample(QtW.QWidget):
 
                 if coord_x <= x_snap:
                     # for row in range(self.scan_size):
-                    for row in range(self.scan_size_x):
+                    for row in range(self.scan_size_r):
                         if coord_y <= y_snap:
-                            # print(f'coord_x = {coord_x}, coord_y = {coord_y}')
-                            # print(f'row = {row}, col = {col}')
+                            print(f'coord_x = {coord_x}, coord_y = {coord_y}')
+                            print(f'row = {row}, col = {col}')
                             x_scan_pos = self.array_pos_x[row][col]
                             y_scan_pos = self.array_pos_y[row][col]
                             z_scan_pos = self.array_pos_z[row][col]
