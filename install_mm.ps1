6059b89b933088f3eecfcf1fd1cf60984edc9c63
Talley Lambert <talley.lambert@gmail.com>
2021-03-06 17:03:42 -0500

    more test

--- install_mm.ps1
+++ install_mm.ps1
@@ -1,6 +1,7 @@
-mkdir mm
+md mm
 $ProgressPreference = 'SilentlyContinue'
 $d=20201215  # known to work
 $url="https://valelab4.ucsf.edu/~MM/nightlyBuilds/2.0.0-gamma/Windows/MMSetup_64bit_2.0.0-gamma1_$d.exe"
 Invoke-WebRequest -Uri $url -OutFile .\mm\mm.exe
 .\mm\mm.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR=".\mm\Micro-Manager"
+Write-Host "done"
