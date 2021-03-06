ba9d8070561c411a7507d0b43b6b6893c5246600
Talley Lambert <talley.lambert@gmail.com>
2021-03-06 17:11:40 -0500

    force wait

--- install_mm.ps1
+++ install_mm.ps1
@@ -3,5 +3,5 @@ $ProgressPreference = 'SilentlyContinue'
 $d=20201215  # known to work
 $url="https://valelab4.ucsf.edu/~MM/nightlyBuilds/2.0.0-gamma/Windows/MMSetup_64bit_2.0.0-gamma1_$d.exe"
 Invoke-WebRequest -Uri $url -OutFile .\mm\mm.exe
-.\mm\mm.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR=".\mm\Micro-Manager"
+.\mm\mm.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR=".\mm\Micro-Manager" | Out-Null
 Write-Host "done"
