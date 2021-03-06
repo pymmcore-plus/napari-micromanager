878500e54740c64be02d3297537da4beee436b62
Talley Lambert <talley.lambert@gmail.com>
2021-03-06 16:57:19 -0500

    use invoke-web

--- install_mm.ps1
+++ install_mm.ps1
@@ -2,5 +2,5 @@ mkdir mm
 $ProgressPreference = 'SilentlyContinue'
 $d=20201215  # known to work
 $url="https://valelab4.ucsf.edu/~MM/nightlyBuilds/2.0.0-gamma/Windows/MMSetup_64bit_2.0.0-gamma1_$d.exe"
-wget $url -o mm\mm.exe
+Invoke-WebRequest -Uri $url -OutFile .\mm\mm.exe
 .\mm\mm.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /DIR=".\mm\Micro-Manager"
