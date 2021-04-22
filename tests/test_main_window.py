b706308133237e90e2c490cb466a964e3fcdb57a
Talley Lambert <talley.lambert@gmail.com>
2021-04-22 10:35:12 -0400

    fix test

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -1,5 +1,5 @@
 import pytest
-from micromanager_gui.qmmcore import QMMCore
+from micromanager_gui.qmmcore import mmcore
 
 # tests are critical if you want to be able to build on things
 # without breaking previous functionality.  Keep tests as small
@@ -26,55 +26,52 @@ def main_window(qtbot):
 # here, we use the `main_window` fixture defined above.
 def test_load_default_config(main_window):
     """Test that load_cfg loads the default config."""
-    mmcore = QMMCore()
 
     # the main idea in a test is to "assert" things you expect
     # to be true. Here we assert the demo_config (which was loaded
     # in the main_window fixture. will load these devices:
     expected = {
         "DHub",
-        mmcore.getCameraDevice(),
+        mmcore.getCameraDevice().result(),
         "Objective",
-        mmcore.getFocusDevice(),
-        mmcore.getXYStageDevice(),
+        mmcore.getFocusDevice().result(),
+        mmcore.getXYStageDevice().result(),
         "Core",
     }
-    assert expected.issubset(set(mmcore.getLoadedDevices()))
+    assert expected.issubset(set(mmcore.getLoadedDevices().result()))
 
 
 def test_stage_xy_position(main_window):
     """Test that we can move the stage."""
-    mmcore = QMMCore()
-    x, y = mmcore.getXPosition(), mmcore.getYPosition()
+    x, y = mmcore.getXPosition().result(), mmcore.getYPosition().result()
     assert (x, y) == (0, 0)
 
     xystep = main_window.xy_step_size_SpinBox.value()
 
     main_window.stage_x_left()
-    x, y = int(mmcore.getXPosition()), mmcore.getYPosition()
+    x, y = int(mmcore.getXPosition().result()), mmcore.getYPosition().result()
     assert (x, y) == (-xystep, 0)
 
     main_window.stage_x_right()
-    x, y = int(mmcore.getXPosition()), mmcore.getYPosition()
+    x, y = int(mmcore.getXPosition().result()), mmcore.getYPosition().result()
     assert (x, y) == (0, 0)
 
     main_window.stage_y_up()
-    x, y = mmcore.getXPosition(), int(mmcore.getYPosition())
+    x, y = mmcore.getXPosition().result(), int(mmcore.getYPosition().result())
     assert (x, y) == (0, xystep)
 
     main_window.stage_y_down()
-    x, y = mmcore.getXPosition(), int(mmcore.getYPosition())
+    x, y = mmcore.getXPosition().result(), int(mmcore.getYPosition().result())
     assert (x, y) == (0, 0)
 
 
 def test_stage_z_position(main_window):
-    mmcore = QMMCore()
-    zpos = mmcore.getPosition(mmcore.getFocusDevice())
+    zpos = mmcore.getPosition(mmcore.getFocusDevice().result()).result()
     assert zpos == 0
 
     main_window.stage_z_up()
     expected = main_window.z_step_size_doubleSpinBox.value()
-    assert mmcore.getPosition(mmcore.getFocusDevice()) == expected
+    assert mmcore.getPosition(mmcore.getFocusDevice().result()).result() == expected
 
     main_window.stage_z_down()
-    assert mmcore.getPosition(mmcore.getFocusDevice()) == 0
+    assert mmcore.getPosition(mmcore.getFocusDevice().result()).result() == 0
