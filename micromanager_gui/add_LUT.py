2f4818484cb1306a68b2a0612203407193b7f3e6
fdrgsp <federico.gasparoli@gmail.com>
2021-06-24 00:55:49 -0400

    add hide_show

--- micromanager_gui/add_LUT.py
+++ micromanager_gui/add_LUT.py
@@ -22,7 +22,7 @@ if TYPE_CHECKING:
 
 @napari_hook_implementation
 def napari_experimental_provide_function():
-    return add_lut
+    return [add_lut, hide_show]
 
 class Color(Enum):
     gray = 'gray'
@@ -32,10 +32,19 @@ class Color(Enum):
     cyan = 'cyan'
 
 def add_lut(viewer: 'napari.viewer.Viewer', operation: Color):
-    """Adds LUT."""
+    """Add LUT to selected layers."""
 
     for l in viewer.layers.selection:
         l.colormap = operation.value
     return
 
+class HideShow(Enum):
+    hide = False
+    show = True
+    
+def hide_show(viewer: 'napari.viewer.Viewer', operation: HideShow):
+    """Hide/Show selected layers."""
 
+    for l in viewer.layers.selection:
+        l.visible = operation.value
+    return
