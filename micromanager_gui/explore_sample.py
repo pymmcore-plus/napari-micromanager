caa326756b29384fe423f70273333f6ff7f87a03
fdrgsp <federico.gasparoli@gmail.com>
2021-06-30 22:51:30 -0400

    clicked.connect(lambda e: self._mmc.cancel())

--- micromanager_gui/explore_sample.py
+++ micromanager_gui/explore_sample.py
@@ -49,7 +49,7 @@ class ExploreSample(QtW.QWidget):
         self.move_to_Button.clicked.connect(self.move_to)
         self.browse_save_explorer_Button.clicked.connect(self.set_explorer_dir)
 
-        self.stop_scan_Button.pressed.connect(self._mmc.cancel)
+        self.stop_scan_Button.clicked.connect(lambda e: self._mmc.cancel())
 
     def enable_explorer_groupbox(self):
         self.scan_size_spinBox_r.setEnabled(True)
