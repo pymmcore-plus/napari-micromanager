ec52a9a75f6e0516d1c2d7c231457c301d04da0c
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2021-09-02 13:23:41 -0400

    relative path for config

--- launch-dev.py
+++ launch-dev.py
@@ -1,9 +1,10 @@
 import napari
+from pathlib import Path
 
 v = napari.Viewer()
 dw, main_window = v.window.add_plugin_dock_widget("micromanager")
 
 core = main_window._mmc
-core.loadSystemConfiguration("tests/test_config.cfg")
+core.loadSystemConfiguration(str(Path(__file__).parent / "tests"/"test_config.cfg"))
 
 napari.run()
