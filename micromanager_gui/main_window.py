59afa675b51535c60d2e800615175768881a6c2f
fdrgsp <federico.gasparoli@gmail.com>
2021-09-15 18:55:07 -0400

    .prop_browser

--- micromanager_gui/main_window.py
+++ micromanager_gui/main_window.py
@@ -11,12 +11,11 @@ from qtpy import uic
 from qtpy.QtCore import QSize, QTimer
 from qtpy.QtGui import QIcon
 
-from prop_browser import PropBrowser
-
 from ._saving import save_sequence
 from ._util import blockSignals, event_indices, extend_array_for_index
 from .explore_sample import ExploreSample
 from .multid_widget import MultiDWidget, SequenceMeta
+from .prop_browser import PropBrowser
 
 if TYPE_CHECKING:
     import napari.layers
