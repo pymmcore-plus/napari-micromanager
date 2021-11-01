181999ed7571182ccf95de14a88ec4931b147e52
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2021-11-01 12:55:08 -0400

    update mda pos table

--- micromanager_gui/multid_widget.py
+++ micromanager_gui/multid_widget.py
@@ -199,9 +199,7 @@ class MultiDWidget(QtW.QWidget, _MultiDUI):
             self.stage_tableWidget.setItem(idx, 2, QtW.QTableWidgetItem(z_txt))
 
             if self._mmc.getAutoFocusDevice() and self._mmc.isContinuousFocusEnabled():
-                z_offset = self._mmc.getProperty(
-                    self._mmc.getAutoFocusDevice(), "Position"
-                )
+                z_offset = self._mmc.getProperty("TIPFSOffset", "Position")  # for Nikon PFS only for now
                 z_offset_txt = QtW.QTableWidgetItem(str(z_offset))
                 z_offset_txt.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
                 self.stage_tableWidget.setItem(
