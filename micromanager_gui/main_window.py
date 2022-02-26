042fdb6e71bacf5002e81d8141e560754f054e5e
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2022-02-26 12:53:00 -0500

    _on_system_cfg_loaded

--- micromanager_gui/main_window.py
+++ micromanager_gui/main_window.py
@@ -95,7 +95,7 @@ class MainWindow(MicroManagerWidget):
         # to core may outlive the lifetime of this particular widget.
         sig.sequenceStarted.connect(self._on_mda_started)
         sig.sequenceFinished.connect(self._on_mda_finished)
-        sig.systemConfigurationLoaded.connect(self._refresh_options)
+        sig.systemConfigurationLoaded.connect(self._on_system_cfg_loaded)
         sig.XYStagePositionChanged.connect(self._on_xy_stage_position_changed)
         sig.stagePositionChanged.connect(self._on_stage_position_changed)
         sig.exposureChanged.connect(self._on_exp_change)
@@ -139,6 +139,11 @@ class MainWindow(MicroManagerWidget):
         self.viewer.layers.events.connect(self.update_max_min)
         self.viewer.layers.selection.events.active.connect(self.update_max_min)
         self.viewer.dims.events.current_step.connect(self.update_max_min)
+    
+    def _on_system_cfg_loaded(self):
+        if len(self._mmc.getLoadedDevices()) > 1:
+            self._set_enabled(True)
+            self._refresh_options()
 
     def _set_enabled(self, enabled):
 
@@ -214,8 +219,6 @@ class MainWindow(MicroManagerWidget):
         if cfg == "":
             cfg = "MMConfig_demo.cfg"
         self._mmc.loadSystemConfiguration(cfg)
-        self._refresh_options()
-        self._set_enabled(True)
 
     def _refresh_options(self):
         # self._refresh_camera_options()
