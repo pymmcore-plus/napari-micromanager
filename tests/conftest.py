c7a28903c81cbe3e3dd15e31a4edfcc3519282f3
Talley Lambert <talley.lambert@gmail.com>
2022-02-28 13:10:51 -0500

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,13 +31,7 @@ def main_window(request, make_napari_viewer):
     config_path = os.path.dirname(os.path.abspath(__file__)) + "/test_config.cfg"
     win.cfg_wdg.cfg_LineEdit.setText(config_path)
     win._mmc.loadSystemConfiguration(config_path)
-
-    win._set_enabled(True)
-
-    try:
-        yield win
-    finally:
-        viewer.close()
+    return win
 
 
 @pytest.fixture
