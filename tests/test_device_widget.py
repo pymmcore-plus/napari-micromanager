fcf8d003ae7cbb147bc3de890d761a6f6c550251
Talley Lambert <talley.lambert@gmail.com>
2022-03-02 19:20:05 -0500

    change test, requires change to pymmcore+

--- tests/test_device_widget.py
+++ tests/test_device_widget.py
@@ -1,34 +1,32 @@
-from pymmcore_plus import CMMCorePlus
+from pymmcore_plus import CMMCorePlus, DeviceType
 
 from micromanager_gui._gui_objects._device_widget import DeviceWidget, StateDeviceWidget
 
 
 def test_state_device_widget(qtbot, global_mmcore: CMMCorePlus):
-    # FIXME? not sure if possible, but this test won't work for devices that
-    # don't broadcast their change with an event from core.
-    # for label in global_mmcore.getLoadedDevicesOfType(DeviceType.StateDevice):
-    # so, for now, just test objective
-    label = "Objective"
+    for label in global_mmcore.getLoadedDevicesOfType(DeviceType.StateDevice):
+        wdg: StateDeviceWidget = DeviceWidget.for_device(label)
+        qtbot.addWidget(wdg)
+        wdg.show()
+        assert wdg.deviceLabel() == label
+        # assert wdg.deviceName() == "DObjective"
+        assert global_mmcore.getStateLabel(label) == wdg._combo.currentText()
+        assert global_mmcore.getState(label) == wdg._combo.currentIndex()
+        start_state = wdg.state()
 
-    wdg: StateDeviceWidget = DeviceWidget.for_device(label)
-    qtbot.addWidget(wdg)
-    wdg.show()
-    assert wdg.deviceLabel() == label
-    assert wdg.deviceName() == "DObjective"
-    assert global_mmcore.getStateLabel(label) == wdg._combo.currentText()
-    assert global_mmcore.getState(label) == wdg._combo.currentIndex()
-    start_state = wdg.state()
+        next_state = (wdg.state() + 1) % len(wdg.stateLabels())
+        with qtbot.waitSignal(global_mmcore.events.propertyChanged):
+            global_mmcore.setState(label, next_state)
 
-    next_state = (wdg.state() + 1) % len(wdg.stateLabels())
-    with qtbot.waitSignal(global_mmcore.events.propertyChanged):
-        global_mmcore.setState(label, next_state)
+        assert wdg.state() != start_state
+        assert wdg.state() == global_mmcore.getState(label) == wdg._combo.currentIndex()
+        assert (
+            wdg.stateLabel()
+            == global_mmcore.getStateLabel(label)
+            == wdg._combo.currentText()
+        )
 
-    assert wdg.state() != start_state
-    assert wdg.state() == global_mmcore.getState(label) == wdg._combo.currentIndex()
-    assert (
-        wdg.stateLabel()
-        == global_mmcore.getStateLabel(label)
-        == wdg._combo.currentText()
-    )
-
-    wdg._disconnect()
+        wdg._disconnect()
+        # once disconnected, core changes shouldn't call out to the widget
+        global_mmcore.setState(label, start_state)
+        assert global_mmcore.getStateLabel(label) != wdg._combo.currentText()
