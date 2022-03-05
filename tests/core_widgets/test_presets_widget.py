b22d1e1b2c6e001d0d496e2149c9bf9d1b44314c
fdrgsp <federico.gasparoli@gmail.com>
2022-03-05 11:01:31 -0500

    update tests

--- tests/core_widgets/test_presets_widget.py
+++ tests/core_widgets/test_presets_widget.py
@@ -1,33 +1,25 @@
-from pymmcore_plus import CMMCorePlus
-
 from micromanager_gui._core_widgets._presets_widget import PresetsWidget
 
 
-def test_preset_widget(qtbot, global_mmcore: CMMCorePlus):
-
-    groups = list(global_mmcore.getAvailableConfigGroups())
-
-    for group in groups:
-
+def test_preset_widget(qtbot, global_mmcore):
+    for group in global_mmcore.getAvailableConfigGroups():
+        wdg = PresetsWidget(group)
+        qtbot.addWidget(wdg)
         presets = list(global_mmcore.getAvailableConfigs(group))
+        assert list(wdg.allowedValues()) == presets
 
+        # no need testing the changes of a config group that has <= 1 item
         if len(presets) <= 1:
             return
 
-        wdg = PresetsWidget(group)
-        qtbot.addWidget(wdg)
-        wdg.show()
-
-        assert list(wdg.allowedValues()) == presets
-
         with qtbot.waitSignal(global_mmcore.events.configSet):
-            global_mmcore.setConfig(group, presets[1])
-        assert wdg.value() == presets[1]
+            global_mmcore.setConfig(group, presets[-1])
+        assert wdg.value() == presets[-1] == global_mmcore.getCurrentConfig(group)
 
         wdg.setValue(presets[0])
         assert global_mmcore.getCurrentConfig(group) == presets[0]
 
         wdg._disconnect()
         # once disconnected, core changes shouldn't call out to the widget
-        global_mmcore.setConfig(group, presets[1])
+        global_mmcore.setConfig(group, presets[-1])
         assert global_mmcore.getCurrentConfig(group) != wdg.value()
