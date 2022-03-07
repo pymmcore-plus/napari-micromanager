9c6f4adf638f8875af6197066201a19e54f16c4a
fdrgsp <federico.gasparoli@gmail.com>
2022-03-07 14:44:07 -0500

    fix test

--- tests/test_group_preset_widget.py
+++ tests/test_group_preset_widget.py
@@ -6,7 +6,7 @@ def test_populating_group_preset_table(main_window: MainWindow, qtbot):
     mmc = main_window._mmc
     table = main_window.group_preset_table_wdg.table_wdg
 
-    assert len(list(mmc.getAvailableConfigGroups())) == 8
+    assert len(list(mmc.getAvailableConfigGroups())) == 9
 
     for r in range(table.rowCount()):
 
@@ -23,9 +23,16 @@ def test_populating_group_preset_table(main_window: MainWindow, qtbot):
             assert wdg.value() == "DAPI"
 
         elif group_name == "_combobox_no_preset_test":
-            assert set(wdg.allowedValues()) == {"1", "2", "4", "8"}
+            assert set(wdg.allowedValues()) == {
+                "8",
+                "16",
+                "10",
+                "32",
+                "14",
+                "12",
+            }
             wdg.setValue("8")
-            assert mmc.getProperty("Camera", "Binning") == "8"
+            assert mmc.getProperty("Camera", "BitDepth") == "8"
 
         elif group_name == "_lineedit_test":
             assert str(wdg.value()) in {"512", "512.0"}
