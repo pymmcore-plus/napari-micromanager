1be38342999248c7724357170a2b290d5b732b73
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-03-07 18:52:18 -0500

    Begin cleanup and decoupling of mda (#134)
    * move mda to gui folder
    
    Required some extra imports in explore-sample. Will remove in
    a subsequent commit that decouples them.
    
    * update console in launch dev script
    
    * fix typing
    
    * show mda when initiated from a script
    
    * fix test
    
    * update basic mda test
    
    * add test of mda started outside napari
    
    * most sequence_meta into it's own file
    
    * move explorer into subwidget - decouple from napari
    
    * Update explorer tests
    
    Use the GUI interactions to trigger the actions.
    
    * make mda script test more robust
    
    * fix typing import
    
    * fix typing
    
    * Update micromanager_gui/main_window.py
    
    Co-authored-by: federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
    
    * move explorer callback in method on object
    
    * set up explorer in dev script
    
    Co-authored-by: federico gasparoli <70725613+fdrgsp@users.noreply.github.com>

--- micromanager_gui/_saving.py
+++ micromanager_gui/_saving.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from napari.components import LayerList
     from useq import MDASequence
 
-    from .multid_widget import SequenceMeta
+    from ._gui_objects._mda_widget import SequenceMeta
 
 
 def _imsave(file: Path, data: np.ndarray, dtype="uint16"):
