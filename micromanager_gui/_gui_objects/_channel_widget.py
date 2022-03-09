d7592dfdb900aab7d7fff0c444d8b63b7920421f
fdrgsp <federico.gasparoli@gmail.com>
2022-03-09 11:48:12 -0500

    update

--- micromanager_gui/_gui_objects/_channel_widget.py
+++ micromanager_gui/_gui_objects/_channel_widget.py
@@ -1,9 +1,10 @@
-from typing import Optional
+from typing import Optional, Union
 
 from pymmcore_plus import CMMCorePlus
-from qtpy.QtWidgets import QWidget
+from qtpy.QtWidgets import QApplication, QComboBox, QVBoxLayout, QWidget
 
 from micromanager_gui import _core
+from micromanager_gui._core_widgets._presets_widget import PresetsWidget
 from micromanager_gui._util import ComboMessageBox
 
 
@@ -12,9 +13,9 @@ class ChannelWidget(QWidget):
 
     Parameters
     ----------
-    objective_device : Optional[str]
-        Device label for the objective device, by default will be guessed using
-        `mmc.guessObjectiveDevices`, and a dialog will be presented if there are
+    channel_group : Optional[str]
+        Name of the group defining the microscope channels, by default will be guessed
+        using `mmc.getOrGuessChannelGroup`, and a dialog will be presented if there are
         multiples
     parent : Optional[QWidget]
         Optional parent widget, by default None
@@ -22,7 +23,7 @@ class ChannelWidget(QWidget):
 
     def __init__(
         self,
-        channel_group: str = None,
+        channel_group: Optional[str] = None,
         parent: Optional[QWidget] = None,
         mmcore: Optional[CMMCorePlus] = None,
     ):
@@ -32,52 +33,66 @@ class ChannelWidget(QWidget):
 
         self._channel_group = channel_group or self._get_channel_group()
 
+        self.channel_combo = self._create_channel_widget(self._channel_group)
+
+        self.setLayout(QVBoxLayout())
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        self.layout().setSpacing(0)
+        self.layout().addWidget(self.channel_combo)
+
+        self._mmc.events.systemConfigurationLoaded.connect(self._on_sys_cfg_loaded)
+        self._mmc.events.channelGroupChanged.connect(self._on_channel_group_changed)
+        self.destroyed.connect(self._disconnect_from_core)
+        self._on_sys_cfg_loaded()
+
     def _get_channel_group(self) -> Optional[str]:
-
         candidates = self._mmc.getOrGuessChannelGroup()
-
         if len(candidates) == 1:
             return candidates[0]
         elif candidates:
-            dialog = ComboMessageBox(candidates, "Select Objective Device:", self)
+            dialog = ComboMessageBox(candidates, "Select Channel Group:", self)
             if dialog.exec_() == dialog.DialogCode.Accepted:
                 return dialog.currentText()
         return None
 
-    # def _refresh_channel_list(self):
-    #     guessed_channel_list = self._mmc.getOrGuessChannelGroup()
+    def _create_channel_widget(
+        self, channel_group: str
+    ) -> Union[PresetsWidget, QComboBox]:
+        if channel_group:
+            channel_combo = PresetsWidget(channel_group)
+        else:
+            channel_combo = QComboBox()
+            channel_combo.setEnabled(False)
+        return channel_combo
 
-    #     if not guessed_channel_list:
-    #         return
+    def _on_sys_cfg_loaded(self):
+        if isinstance(self.channel_combo, QComboBox):
+            self._clear_previous_device_widget()
+            self.channel_combo = self._create_channel_widget(self._channel_group)
+            self.layout().addWidget(self.channel_combo)
 
-    #     if len(guessed_channel_list) == 1:
-    #         self._set_channel_group(guessed_channel_list[0])
-    #     else:
-    #         # if guessed_channel_list has more than 1 possible channel group,
-    #         # you can select the correct one through a combobox
-    #         ch = SelectDeviceFromCombobox(
-    #             guessed_channel_list,
-    #             "Select Channel Group:",
-    #             self,
-    #         )
-    #         ch.val_changed.connect(self._set_channel_group)
-    #         ch.show()
+    def _on_channel_group_changed(self, new_channel_group: str):
+        """When Channel group is changed, recreate combo."""
+        if isinstance(self.channel_combo, QComboBox):
+            self._clear_previous_device_widget()
+            self.channel_combo = self._create_channel_widget(new_channel_group)
+            self.layout().addWidget(self.channel_combo)
+        else:  # if is a PresetsWidget()
+            self.channel_combo.refresh()
 
-    # def _set_channel_group(self, guessed_channel: str):
-    #     channel_group = guessed_channel
-    #     self._mmc.setChannelGroup(channel_group)
-    #     channel_list = self._mmc.getAvailableConfigs(channel_group)
-    #     with blockSignals(self.tab_wdg.snap_channel_comboBox):
-    #         self.tab_wdg.snap_channel_comboBox.clear()
-    #         self.tab_wdg.snap_channel_comboBox.addItems(channel_list)
-    #         self.tab_wdg.snap_channel_comboBox.setCurrentText(
-    #             self._mmc.getCurrentConfig(channel_group)
-    #         )
+    def _clear_previous_device_widget(self):
+        self.channel_combo.setParent(None)
+        self.channel_combo.deleteLater()
 
-    # def _on_config_set(self, groupName: str, configName: str):
-    #     if groupName == self._mmc.getOrGuessChannelGroup():
-    #         with blockSignals(self.tab_wdg.snap_channel_comboBox):
-    #             self.tab_wdg.snap_channel_comboBox.setCurrentText(configName)
+    def _disconnect_from_core(self):
+        self._mmc.events.systemConfigurationLoaded.disconnect(self._on_sys_cfg_loaded)
+        self._mmc.events.channelGroupChanged.disconnect(self._on_channel_group_changed)
 
-    # def _channel_changed(self, newChannel: str):
-    #     self._mmc.setConfig(self._mmc.getChannelGroup(), newChannel)
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    win = ChannelWidget()
+    win.show()
+    sys.exit(app.exec_())
