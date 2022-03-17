1c83d0f1c736fccd6e5469b1e8ac8984c62bb39d
fdrgsp <federico.gasparoli@gmail.com>
2022-03-17 13:03:47 -0400

    _check_if_presets_have_same_props and _set_if_props_match_preset

--- micromanager_gui/_core_widgets/_presets_widget.py
+++ micromanager_gui/_core_widgets/_presets_widget.py
@@ -5,7 +5,7 @@ from qtpy.QtWidgets import QComboBox, QHBoxLayout, QWidget
 from superqt.utils import signals_blocked
 
 from .._core import get_core_singleton
-from .._util import get_dev_prop, get_dev_prop_val, set_wdg_color
+from .._util import get_dev_prop, set_wdg_color
 
 
 class PresetsWidget(QWidget):
@@ -35,6 +35,8 @@ class PresetsWidget(QWidget):
 
         self.dev_prop = get_dev_prop(self._group, self._presets[0])
 
+        self._check_if_presets_have_same_props()
+
         self._combo = QComboBox()
         self._combo.addItems(self._presets)
 
@@ -46,11 +48,18 @@ class PresetsWidget(QWidget):
         self._combo.textActivated.connect(self._on_text_activate)
 
         self._mmc.events.configSet.connect(self._on_cfg_set)
-        self._mmc.events.configSet.connect(self._highlight_if_preset_state_changed)
         self._mmc.events.systemConfigurationLoaded.connect(self.refresh)
         self._mmc.events.propertyChanged.connect(self._highlight_if_prop_changed)
 
         self.destroyed.connect(self.disconnect)
+
+    def _check_if_presets_have_same_props(self):
+        for idx, preset in enumerate(self._presets):
+            if idx == 0:
+                continue
+            device_property = get_dev_prop(self._group, preset)
+            if len(device_property) != len(self.dev_prop):
+                raise ValueError(f"{self._presets} must have the same properties.")
 
     def _on_text_activate(self, text: str):
         """Used in case there is only one preset"""
@@ -67,6 +76,10 @@ class PresetsWidget(QWidget):
             with signals_blocked(self._combo):
                 self._combo.setCurrentText(preset)
                 set_wdg_color(self.text_color, self._combo)
+        else:
+            dev_prop_list = get_dev_prop(group, preset)
+            if any(dev_prop for dev_prop in dev_prop_list if dev_prop in self.dev_prop):
+                self._set_if_props_match_preset()
 
     def value(self) -> str:
         return self._combo.currentText()
@@ -92,19 +105,7 @@ class PresetsWidget(QWidget):
                 self._combo.addItems(presets)
                 self._combo.setEnabled(True)
 
-    def _highlight_if_preset_state_changed(self, group: str, preset: str):
-        """Set the text color to magenta if a preset has changed"""
-
-        if group == self._group:
-            return
-
-        dp = get_dev_prop(group, preset)
-        if any(x for x in dp if x in self.dev_prop):
-            self._set_if_props_match_preset()
-
     def _highlight_if_prop_changed(self, device: str, property: str, value: str):
-        """Set the text color to magenta if a preset property has changed"""
-
         if (device, property) not in self.dev_prop:
             if self._mmc.getDeviceType(device) != DeviceType.StateDevice:
                 return
@@ -112,28 +113,30 @@ class PresetsWidget(QWidget):
             # in dev_prop, we check if the property "State" is in dev_prop.
             if (device, "State") not in self.dev_prop:
                 return
-
         self._set_if_props_match_preset()
 
     def _set_if_props_match_preset(self):
-        for p in self._presets:
+        """
+        Check if a preset matches the current system state.
+        If true, set the combobox to the preset with 'text_color'.
+        If false, set the combobox color to 'magenta'.
+        """
+        for preset in self._presets:
             _set_combo = True
-            dpv = get_dev_prop_val(self._group, p)
-            for i in dpv:
-                cache_prop = self._mmc.getPropertyFromCache(i[0], i[1])
-                if cache_prop != i[2]:
+            for (dev, prop, value) in self._mmc.getConfigData(self._group, preset):
+                cache_value = self._mmc.getPropertyFromCache(dev, prop)
+                if cache_value != value:
                     _set_combo = False
                     break
             if _set_combo:
                 with signals_blocked(self._combo):
-                    self._combo.setCurrentText(p)
+                    self._combo.setCurrentText(preset)
                     set_wdg_color(self.text_color, self._combo)
                     return
-        if not _set_combo:
-            set_wdg_color("magenta", self._combo)
+        # None of the presets match the current system state
+        set_wdg_color("magenta", self._combo)
 
     def disconnect(self):
         self._mmc.events.configSet.disconnect(self._on_cfg_set)
-        self._mmc.events.configSet.disconnect(self._highlight_if_preset_state_changed)
         self._mmc.events.systemConfigurationLoaded.disconnect(self.refresh)
         self._mmc.events.propertyChanged.disconnect(self._highlight_if_prop_changed)
