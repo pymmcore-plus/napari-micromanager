6d852c74048248d3b729ade600332a3b92c36d5e
fdrgsp <federico.gasparoli@gmail.com>
2022-04-17 17:03:31 -0400

    fix test

--- tests/core_widgets/test_presets_widget.py
+++ tests/core_widgets/test_presets_widget.py
@@ -3,7 +3,7 @@ from micromanager_gui._core_widgets._presets_widget import PresetsWidget
 
 def test_preset_widget(qtbot, global_mmcore):
     for group in global_mmcore.getAvailableConfigGroups():
-        wdg = PresetsWidget(group, text_color="white")
+        wdg = PresetsWidget(group)
         qtbot.addWidget(wdg)
         presets = list(global_mmcore.getAvailableConfigs(group))
         assert list(wdg.allowedValues()) == presets
@@ -23,17 +23,17 @@ def test_preset_widget(qtbot, global_mmcore):
             global_mmcore.setProperty("Camera", "Binning", "8")
             assert wdg._combo.styleSheet() == "color: magenta;"
             global_mmcore.setProperty("Camera", "Binning", "1")
-            assert wdg._combo.styleSheet() == "color: white;"
+            assert wdg._combo.styleSheet() == ""
 
             global_mmcore.setConfig("Camera", "HighRes")
             assert wdg._combo.currentText() == "HighRes"
-            assert wdg._combo.styleSheet() == "color: white;"
+            assert wdg._combo.styleSheet() == ""
             global_mmcore.setProperty("Camera", "Binning", "2")
             assert wdg._combo.currentText() == "HighRes"
             assert wdg._combo.styleSheet() == "color: magenta;"
             global_mmcore.setProperty("Camera", "BitDepth", "10")
             assert wdg._combo.currentText() == "MedRes"
-            assert wdg._combo.styleSheet() == "color: white;"
+            assert wdg._combo.styleSheet() == ""
 
         wdg.disconnect()
         # once disconnected, core changes shouldn't call out to the widget
