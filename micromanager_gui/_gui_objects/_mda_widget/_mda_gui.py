b26ce6c8eca864bcdf8f73ab2bbe7b0d84f1174c
fdrgsp <federico.gasparoli@gmail.com>
2022-05-05 14:39:57 -0400

    all widgets

--- micromanager_gui/_gui_objects/_mda_widget/_mda_gui.py
+++ micromanager_gui/_gui_objects/_mda_widget/_mda_gui.py
@@ -3,6 +3,8 @@ from typing import Optional
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import (
     QCheckBox,
+    QComboBox,
+    QDoubleSpinBox,
     QGridLayout,
     QGroupBox,
     QHBoxLayout,
@@ -11,7 +13,9 @@ from qtpy.QtWidgets import (
     QPushButton,
     QScrollArea,
     QSizePolicy,
+    QSpinBox,
     QTableWidget,
+    QTabWidget,
     QVBoxLayout,
     QWidget,
 )
@@ -32,6 +36,9 @@ class MMMDAWidget(QWidget):
         self.explorer_wdg = self._create_gui()
         self._scroll.setWidget(self.explorer_wdg)
         self.layout().addWidget(self._scroll)
+
+        self.bottom_wdg = self._create_bottom_wdg()
+        self.layout().addWidget(self.bottom_wdg)
 
     def _create_gui(self):
         wdg = QWidget()
@@ -74,7 +81,7 @@ class MMMDAWidget(QWidget):
         dir_group = QWidget()
         dir_group_layout = QHBoxLayout()
         dir_group_layout.setSpacing(5)
-        dir_group_layout.setContentsMargins(0, 5, 0, 5)
+        dir_group_layout.setContentsMargins(0, 10, 0, 5)
         dir_group.setLayout(dir_group_layout)
         lbl_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         min_lbl_size = 70
@@ -107,7 +114,6 @@ class MMMDAWidget(QWidget):
         self.checkBox_save_pos = QCheckBox(
             text="Save XY Positions in separate files (ImageJ compatibility)"
         )
-        # self.checkBox_save_pos.
 
         group_layout.addWidget(dir_group)
         group_layout.addWidget(fname_group)
@@ -126,46 +132,284 @@ class MMMDAWidget(QWidget):
         group.setLayout(group_layout)
 
         # table
-        self.channel_explorer_tableWidget = QTableWidget()
-        self.channel_explorer_tableWidget.setMinimumHeight(90)
-        hdr = self.channel_explorer_tableWidget.horizontalHeader()
+        self.channel_tableWidget = QTableWidget()
+        self.channel_tableWidget.setMinimumHeight(90)
+        hdr = self.channel_tableWidget.horizontalHeader()
         hdr.setSectionResizeMode(hdr.Stretch)
-        self.channel_explorer_tableWidget.verticalHeader().setVisible(False)
-        self.channel_explorer_tableWidget.setTabKeyNavigation(True)
-        self.channel_explorer_tableWidget.setColumnCount(2)
-        self.channel_explorer_tableWidget.setRowCount(0)
-        self.channel_explorer_tableWidget.setHorizontalHeaderLabels(
+        self.channel_tableWidget.verticalHeader().setVisible(False)
+        self.channel_tableWidget.setTabKeyNavigation(True)
+        self.channel_tableWidget.setColumnCount(2)
+        self.channel_tableWidget.setRowCount(0)
+        self.channel_tableWidget.setHorizontalHeaderLabels(
             ["Channel", "Exposure Time (ms)"]
         )
-        group_layout.addWidget(self.channel_explorer_tableWidget, 0, 0, 3, 1)
+        group_layout.addWidget(self.channel_tableWidget, 0, 0, 3, 1)
 
         # buttons
         btn_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         min_size = 100
-        self.add_ch_explorer_Button = QPushButton(text="Add")
-        self.add_ch_explorer_Button.setMinimumWidth(min_size)
-        self.add_ch_explorer_Button.setSizePolicy(btn_sizepolicy)
-        self.remove_ch_explorer_Button = QPushButton(text="Remove")
-        self.remove_ch_explorer_Button.setMinimumWidth(min_size)
-        self.remove_ch_explorer_Button.setSizePolicy(btn_sizepolicy)
-        self.clear_ch_explorer_Button = QPushButton(text="Clear")
-        self.clear_ch_explorer_Button.setMinimumWidth(min_size)
-        self.clear_ch_explorer_Button.setSizePolicy(btn_sizepolicy)
+        self.add_ch_Button = QPushButton(text="Add")
+        self.add_ch_Button.setMinimumWidth(min_size)
+        self.add_ch_Button.setSizePolicy(btn_sizepolicy)
+        self.remove_ch_Button = QPushButton(text="Remove")
+        self.remove_ch_Button.setMinimumWidth(min_size)
+        self.remove_ch_Button.setSizePolicy(btn_sizepolicy)
+        self.clear_ch_Button = QPushButton(text="Clear")
+        self.clear_ch_Button.setMinimumWidth(min_size)
+        self.clear_ch_Button.setSizePolicy(btn_sizepolicy)
 
-        group_layout.addWidget(self.add_ch_explorer_Button, 0, 1, 1, 1)
-        group_layout.addWidget(self.remove_ch_explorer_Button, 1, 1, 1, 2)
-        group_layout.addWidget(self.clear_ch_explorer_Button, 2, 1, 1, 2)
+        # checkbox
+        self.checkBox_split_channels = QCheckBox(text="Split Channels")
+
+        group_layout.addWidget(self.add_ch_Button, 0, 1, 1, 1)
+        group_layout.addWidget(self.remove_ch_Button, 1, 1, 1, 2)
+        group_layout.addWidget(self.clear_ch_Button, 2, 1, 1, 2)
+        group_layout.addWidget(self.checkBox_split_channels, 3, 0, 1, 1)
 
         return group
 
     def _create_time_group(self):
-        pass
+        group = QGroupBox(title="Time")
+        group.setCheckable(True)
+        group.setChecked(False)
+        group.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed))
+        group_layout = QHBoxLayout()
+        group_layout.setSpacing(20)
+        group_layout.setContentsMargins(10, 10, 10, 10)
+        group.setLayout(group_layout)
+
+        lbl_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        # Timepoints
+        wdg = QWidget()
+        wdg_lay = QHBoxLayout()
+        wdg_lay.setSpacing(5)
+        wdg_lay.setContentsMargins(0, 0, 0, 0)
+        wdg.setLayout(wdg_lay)
+        lbl = QLabel(text="Timepoints:")
+        lbl.setSizePolicy(lbl_sizepolicy)
+        self.timepoints_spinBox = QSpinBox()
+        self.timepoints_spinBox.setMinimum(1)
+        self.timepoints_spinBox.setMaximum(10000)
+        self.timepoints_spinBox.setAlignment(Qt.AlignCenter)
+        wdg_lay.addWidget(lbl)
+        wdg_lay.addWidget(self.timepoints_spinBox)
+        group_layout.addWidget(wdg)
+
+        # Interval
+        wdg1 = QWidget()
+        wdg1_lay = QHBoxLayout()
+        wdg1_lay.setSpacing(5)
+        wdg1_lay.setContentsMargins(0, 0, 0, 0)
+        wdg1.setLayout(wdg1_lay)
+        lbl1 = QLabel(text="Interval:")
+        lbl1.setSizePolicy(lbl_sizepolicy)
+        self.interval_spinBox = QSpinBox()
+        self.interval_spinBox.setMinimum(0)
+        self.interval_spinBox.setMaximum(10000)
+        self.interval_spinBox.setAlignment(Qt.AlignCenter)
+        wdg1_lay.addWidget(lbl1)
+        wdg1_lay.addWidget(self.interval_spinBox)
+        group_layout.addWidget(wdg1)
+
+        return group
 
     def _create_stack_groupBox(self):
-        pass
+        group = QGroupBox(title="Z Stacks")
+        group.setCheckable(True)
+        group.setChecked(False)
+        group.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed))
+        group_layout = QHBoxLayout()
+        group_layout.setSpacing(0)
+        group_layout.setContentsMargins(10, 10, 10, 10)
+        group.setLayout(group_layout)
+
+        # tab
+        self.z_tabWidget = QTabWidget()
+        z_tab_layout = QVBoxLayout()
+        z_tab_layout.setSpacing(0)
+        z_tab_layout.setContentsMargins(0, 0, 0, 0)
+        self.z_tabWidget.setLayout(z_tab_layout)
+        group_layout.addWidget(self.z_tabWidget)
+
+        # top bottom
+        tb = QWidget()
+        tb_layout = QGridLayout()
+        tb.setLayout(tb_layout)
+
+        self.set_top_Button = QPushButton(text="Set Top")
+        self.set_bottom_Button = QPushButton(text="Set Bottom")
+
+        lbl_range_tb = QLabel(text="Range (µm):")
+        lbl_range_tb.setAlignment(Qt.AlignCenter)
+
+        self.z_top_doubleSpinBox = QDoubleSpinBox()
+        self.z_top_doubleSpinBox.setAlignment(Qt.AlignCenter)
+        self.z_top_doubleSpinBox.setMinimum(0.0)
+        self.z_top_doubleSpinBox.setMaximum(100000)
+        self.z_top_doubleSpinBox.setDecimals(2)
+
+        self.z_bottom_doubleSpinBox = QDoubleSpinBox()
+        self.z_bottom_doubleSpinBox.setAlignment(Qt.AlignCenter)
+        self.z_bottom_doubleSpinBox.setMinimum(0.0)
+        self.z_bottom_doubleSpinBox.setMaximum(100000)
+        self.z_bottom_doubleSpinBox.setDecimals(2)
+
+        self.z_range_topbottom_doubleSpinBox = QDoubleSpinBox()
+        self.z_range_topbottom_doubleSpinBox.setAlignment(Qt.AlignCenter)
+        self.z_range_topbottom_doubleSpinBox.setMaximum(10000000)
+        self.z_range_topbottom_doubleSpinBox.setButtonSymbols(2)
+        self.z_range_topbottom_doubleSpinBox.setReadOnly(True)
+
+        tb_layout.addWidget(self.set_top_Button, 0, 0)
+        tb_layout.addWidget(self.z_top_doubleSpinBox, 1, 0)
+        tb_layout.addWidget(self.set_bottom_Button, 0, 1)
+        tb_layout.addWidget(self.z_bottom_doubleSpinBox, 1, 1)
+        tb_layout.addWidget(lbl_range_tb, 0, 2)
+        tb_layout.addWidget(self.z_range_topbottom_doubleSpinBox, 1, 2)
+
+        self.z_tabWidget.addTab(tb, "TopBottom")
+
+        # range around
+        ra = QWidget()
+        ra_layout = QHBoxLayout()
+        ra_layout.setSpacing(10)
+        ra_layout.setContentsMargins(0, 0, 0, 0)
+        ra.setLayout(ra_layout)
+
+        lbl_range_ra = QLabel(text="Range (µm):")
+        lbl_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        lbl_range_ra.setSizePolicy(lbl_sizepolicy)
+
+        self.zrange_spinBox = QSpinBox()
+        self.zrange_spinBox.setValue(5)
+        self.zrange_spinBox.setAlignment(Qt.AlignCenter)
+        self.zrange_spinBox.setMaximum(100000)
+
+        self.range_around_label = QLabel(text="-2.5 µm <- z -> +2.5 µm")
+        self.range_around_label.setAlignment(Qt.AlignCenter)
+
+        ra_layout.addWidget(lbl_range_ra)
+        ra_layout.addWidget(self.zrange_spinBox)
+        ra_layout.addWidget(self.range_around_label)
+
+        self.z_tabWidget.addTab(ra, "RangeAround")
+
+        # above below wdg
+        ab = QWidget()
+        ab_layout = QGridLayout()
+        ab.setLayout(ab_layout)
+
+        lbl_above = QLabel(text="Above (µm):")
+        lbl_above.setAlignment(Qt.AlignCenter)
+        self.above_doubleSpinBox = QDoubleSpinBox()
+        self.above_doubleSpinBox.setAlignment(Qt.AlignCenter)
+        self.above_doubleSpinBox.setMinimum(0.05)
+        self.above_doubleSpinBox.setMaximum(10000)
+        self.above_doubleSpinBox.setSingleStep(0.5)
+        self.above_doubleSpinBox.setDecimals(2)
+
+        lbl_below = QLabel(text="Below (µm):")
+        lbl_below.setAlignment(Qt.AlignCenter)
+        self.below_doubleSpinBox = QDoubleSpinBox()
+        self.below_doubleSpinBox.setAlignment(Qt.AlignCenter)
+        self.below_doubleSpinBox.setMinimum(0.05)
+        self.below_doubleSpinBox.setMaximum(10000)
+        self.below_doubleSpinBox.setSingleStep(0.5)
+        self.below_doubleSpinBox.setDecimals(2)
+
+        lbl_range = QLabel(text="Range (µm):")
+        lbl_range.setAlignment(Qt.AlignCenter)
+        self.z_range_abovebelow_doubleSpinBox = QDoubleSpinBox()
+        self.z_range_abovebelow_doubleSpinBox.setAlignment(Qt.AlignCenter)
+        self.z_range_abovebelow_doubleSpinBox.setMaximum(10000000)
+        self.z_range_abovebelow_doubleSpinBox.setButtonSymbols(2)
+        self.z_range_abovebelow_doubleSpinBox.setReadOnly(True)
+
+        ab_layout.addWidget(lbl_above, 0, 0)
+        ab_layout.addWidget(self.above_doubleSpinBox, 1, 0)
+        ab_layout.addWidget(lbl_below, 0, 1)
+        ab_layout.addWidget(self.below_doubleSpinBox, 1, 1)
+        ab_layout.addWidget(lbl_range, 0, 2)
+        ab_layout.addWidget(self.z_range_abovebelow_doubleSpinBox, 1, 2)
+
+        self.z_tabWidget.addTab(ab, "AboveBelow")
+
+        return group
 
     def _create_stage_pos_groupBox(self):
-        pass
+        group = QGroupBox(title="Stage Positions (double-click to move to position)")
+        group.setCheckable(True)
+        group.setChecked(False)
+        group.setMinimumHeight(230)
+        group_layout = QGridLayout()
+        group_layout.setHorizontalSpacing(15)
+        group_layout.setVerticalSpacing(0)
+        group_layout.setContentsMargins(10, 0, 10, 0)
+        group.setLayout(group_layout)
+
+        # table
+        self.channel_tableWidget = QTableWidget()
+        self.channel_tableWidget.setMinimumHeight(90)
+        hdr = self.channel_tableWidget.horizontalHeader()
+        hdr.setSectionResizeMode(hdr.Stretch)
+        self.channel_tableWidget.verticalHeader().setVisible(False)
+        self.channel_tableWidget.setTabKeyNavigation(True)
+        self.channel_tableWidget.setColumnCount(3)
+        self.channel_tableWidget.setRowCount(0)
+        self.channel_tableWidget.setHorizontalHeaderLabels(["X", "Y", "Z"])
+        group_layout.addWidget(self.channel_tableWidget, 0, 0, 3, 1)
+
+        # buttons
+        btn_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        min_size = 100
+        self.add_pos_Button = QPushButton(text="Add")
+        self.add_pos_Button.setMinimumWidth(min_size)
+        self.add_pos_Button.setSizePolicy(btn_sizepolicy)
+        self.remove_pos_Button = QPushButton(text="Remove")
+        self.remove_pos_Button.setMinimumWidth(min_size)
+        self.remove_pos_Button.setSizePolicy(btn_sizepolicy)
+        self.clear_pos_Button = QPushButton(text="Clear")
+        self.clear_pos_Button.setMinimumWidth(min_size)
+        self.clear_pos_Button.setSizePolicy(btn_sizepolicy)
+
+        group_layout.addWidget(self.add_pos_Button, 0, 1, 1, 1)
+        group_layout.addWidget(self.remove_pos_Button, 1, 1, 1, 2)
+        group_layout.addWidget(self.clear_pos_Button, 2, 1, 1, 2)
+
+        return group
+
+    def _create_bottom_wdg(self):
+
+        wdg = QWidget()
+        wdg_layout = QHBoxLayout()
+        wdg_layout.setSpacing(10)
+        wdg_layout.setContentsMargins(0, 0, 0, 0)
+        wdg.setLayout(wdg_layout)
+
+        acq_wdg = QWidget()
+        acq_wdg_layout = QHBoxLayout()
+        acq_wdg_layout.setSpacing(0)
+        acq_wdg_layout.setContentsMargins(0, 0, 0, 0)
+        acq_wdg.setLayout(acq_wdg_layout)
+        acquisition_order_label = QLabel(text="Acquisition Order:")
+        lbl_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        acquisition_order_label.setSizePolicy(lbl_sizepolicy)
+        self.acquisition_order_comboBox = QComboBox()
+        self.acquisition_order_comboBox.addItems(["tpzc", "tpcz", "ptzc", "ptcz"])
+        acq_wdg_layout.addWidget(acquisition_order_label)
+        acq_wdg_layout.addWidget(self.acquisition_order_comboBox)
+
+        self.run_Button = QPushButton(text="Run")
+        self.pause_Button = QPushButton("Pause")
+        self.cancel_Button = QPushButton("Cancel")
+
+        wdg_layout.addWidget(acq_wdg)
+        wdg_layout.addWidget(self.run_Button)
+        wdg_layout.addWidget(self.pause_Button)
+        wdg_layout.addWidget(self.cancel_Button)
+
+        return wdg
 
 
 if __name__ == "__main__":
