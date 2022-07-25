700cd2e60822a5a753c7e2530d3595b8acd26d3b
fdrgsp <federico.gasparoli@gmail.com>
2022-07-25 10:39:50 -0400

    fix: remove SnapButton args

--- micromanager_gui/_gui_objects/_tab_widget.py
+++ micromanager_gui/_gui_objects/_tab_widget.py
@@ -67,9 +67,7 @@ class MMTabWidget(QtW.QWidget):
         self.btn_wdg = QtW.QWidget()
         self.btn_wdg.setMaximumHeight(65)
         self.btn_wdg_layout = QtW.QHBoxLayout()
-        self.snap_Button = SnapButton(
-            button_text="Snap", icon_size=40, icon_color=(0, 255, 0)
-        )
+        self.snap_Button = SnapButton()
         self.snap_Button.setMinimumSize(QtCore.QSize(200, 50))
         self.snap_Button.setMaximumSize(QtCore.QSize(200, 50))
         self.btn_wdg_layout.addWidget(self.snap_Button)
