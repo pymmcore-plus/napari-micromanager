ae8c3fa89be0a8a6c43efd0a7cbc5cfdf6827b02
fdrgsp <federico.gasparoli@gmail.com>
2022-12-03 23:47:29 -0500

    feat: use QToolBar instead of menu

--- src/napari_micromanager/__main__.py
+++ src/napari_micromanager/__main__.py
@@ -3,7 +3,5 @@ from napari_micromanager.main_window import MainWindow
 
 viewer = napari.Viewer()
 win = MainWindow(viewer)
-viewer.window.add_dock_widget(
-    win, name="MicroManager", area="right", allowed_areas=["left", "right"]
-)._close_btn = False
+viewer.window.add_dock_widget(win, name="MicroManager", area="top")._close_btn = False
 napari.run()
