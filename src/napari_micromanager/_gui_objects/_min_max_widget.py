10dd25fea6ac4897d4ade0cceeac9addeecd6dd3
Talley Lambert <talley.lambert@gmail.com>
2022-12-06 10:53:29 -0500

    refactor: minmax

--- src/napari_micromanager/_gui_objects/_min_max_widget.py
+++ src/napari_micromanager/_gui_objects/_min_max_widget.py
@@ -1,34 +1,49 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterable
 
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QHBoxLayout, QLabel, QScrollArea, QWidget
+from qtpy.QtGui import QColor
+from qtpy.QtWidgets import QHBoxLayout, QLabel, QScrollArea, QSizePolicy, QWidget
+
+if TYPE_CHECKING:
+    from napari.layers import Image
+
+QCOLORS = set(QColor.colorNames())
 
 
-class MinMax(QWidget):
+class MinMax(QScrollArea):
     """A Widget to display min and max layer grey values."""
 
-    def __init__(self, *, parent: Optional[QWidget] = None) -> None:
+    def __init__(self, *, parent: QWidget | None = None) -> None:
         super().__init__(parent=parent)
+        self.setWidgetResizable(True)
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().setContentsMargins(0, 0, 0, 0)
+        self._label = QLabel()
 
-        scroll = QScrollArea()
-        scroll.setWidgetResizable(True)
-        scroll.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        lbl = QLabel("(min, max)")
+        lbl.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
-        max_min_wdg = QWidget()
-        max_min_wdg_layout = QHBoxLayout()
-        max_min_wdg_layout.setContentsMargins(0, 0, 0, 0)
-        max_min_wdg.setLayout(max_min_wdg_layout)
+        central = QWidget()
+        central.setLayout(QHBoxLayout())
+        central.layout().setContentsMargins(0, 0, 0, 0)
+        central.layout().addWidget(lbl)
+        central.layout().addWidget(self._label)
+        self.setWidget(central)
 
-        self.max_min_val_label_name = QLabel()
-        self.max_min_val_label_name.setText("(min, max)")
-        self.max_min_val_label_name.setMaximumWidth(70)
-        max_min_wdg_layout.addWidget(self.max_min_val_label_name)
+    def update_from_layers(self, layers: Iterable[Image]) -> None:
+        """Update the minmax label based on data from layers."""
+        min_max_txt = ""
+        for layer in layers:
+            col = col if (col := layer.colormap.name) in QCOLORS else "gray"
+            try:
+                minmax = tuple(layer._calc_data_range(mode="slice"))
+            except Exception:
+                import warnings
 
-        self.max_min_val_label = QLabel()
-        max_min_wdg_layout.addWidget(self.max_min_val_label)
+                warnings.warn("cannot update minmax. napari api changed?")
+                continue
+            min_max_txt += f'<font color="{col}">{minmax}</font>'
 
-        scroll.setWidget(max_min_wdg)
-        self.layout().addWidget(scroll)
+        self._label.setText(min_max_txt)
