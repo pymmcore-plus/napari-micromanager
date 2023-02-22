aeca2607fab85bf38202cedfff76310072540dd4
Talley Lambert <talley.lambert@gmail.com>
2023-02-22 18:48:58 -0500

    fix: fix warning on private access (#254)

--- src/napari_micromanager/main_window.py
+++ src/napari_micromanager/main_window.py
@@ -36,7 +36,6 @@ class MainWindow(MicroManagerToolbar):
 
         # get global CMMCorePlus instance
         self._mmc = CMMCorePlus.instance()
-        self.viewer = viewer
 
         self._mda_handler = _NapariMDAHandler(self._mmc, viewer)
         self.streaming_timer: QTimer | None = None
