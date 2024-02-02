41a9943c476cfa52badcbe303424e637679f5f0e
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2024-02-02 14:59:56 -0500

    feat: Allow passing a config from command line (#257)
    * ENH: Allow passing a config from command line
    
    * enh: address review
    
    * test: update tests for new cli args
    
    * test: use warnings instead of napari for pytest
    
    * fixup
    
    ---------
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- src/napari_micromanager/__main__.py
+++ src/napari_micromanager/__main__.py
@@ -1,14 +1,33 @@
 """Run napari-micromanager as a script with ``python -m napari_micromanager``."""
+from __future__ import annotations
+
+import argparse
+import sys
+from typing import Sequence
 
 
-def main() -> None:
+def main(args: Sequence[str] | None = None) -> None:
     """Create a napari viewer and add the MicroManager plugin to it."""
+    if args is None:
+        args = sys.argv
+
+    parser = argparse.ArgumentParser(description="Enter string")
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default=None,
+        help="Config file to load",
+        nargs="?",
+    )
+    parsed_args = parser.parse_args(args)
+
     import napari
 
     from napari_micromanager.main_window import MainWindow
 
     viewer = napari.Viewer()
-    win = MainWindow(viewer)
+    win = MainWindow(viewer, config=parsed_args.config)
     dw = viewer.window.add_dock_widget(win, name="MicroManager", area="top")
     if hasattr(dw, "_close_btn"):
         dw._close_btn = False
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,36 @@
+from pathlib import Path
 from unittest.mock import patch
 
+import pytest
 from napari_micromanager.__main__ import main
+from pymmcore_plus import CMMCorePlus
 
 
-def test_cli_main() -> None:
+@pytest.mark.parametrize(
+    "argv",
+    [
+        [],
+        ["--config", str(Path(__file__).parent / "test_config.cfg")],
+        ["-c", "nonexistant"],
+    ],
+)
+def test_cli_main(argv: list) -> None:
     import napari
     from napari.qt import QtViewer
 
     with patch("napari.run") as mock_run:
         with patch("qtpy.QtWidgets.QMainWindow.show") as mock_show:
-            main()
+            if "nonexistant" in argv:
+                with pytest.warns():
+                    main(argv)
+            else:
+                main(argv)
+
     mock_run.assert_called_once()
     mock_show.assert_called_once()
+
+    if argv and "test_config" in argv[-1]:
+        assert len(CMMCorePlus.instance().getLoadedDevices()) > 1
 
     # this is to prevent a leaked widget error in the NEXT test
     napari.current_viewer().close()
