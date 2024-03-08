097b135e05af9e16dc24c9d779b50725aaed526b
fdrgsp <federico.gasparoli@gmail.com>
2024-03-07 22:39:16 -0500

    test: update test_config_init

--- tests/test_configurations.py
+++ tests/test_configurations.py
@@ -21,7 +21,8 @@ configs = [None, Path(__file__).parent / "test_config.cfg"]
 
 
 @pytest.mark.parametrize("config", configs)
-def test_config_init(qtbot: QtBot, core: CMMCorePlus, config: Path | None):
+@pytest.mark.parametrize("wiz", [True, False])
+def test_config_init(qtbot: QtBot, core: CMMCorePlus, config: Path | None, wiz: bool):
     assert not USER_CONFIGS_PATHS.exists()
 
     init = InitializeSystemConfigurations(mmcore=core, config=config)
@@ -32,23 +33,32 @@ def test_config_init(qtbot: QtBot, core: CMMCorePlus, config: Path | None):
         assert DEMO in [Path(path).name for path in data["paths"]]
 
         if config is None:
-            assert len(data["paths"]) == 1
             assert init._startup_dialog.isVisible()
             combo = init._startup_dialog.cfg_combo
             current_cfg = combo.currentText()
             assert Path(current_cfg).name == DEMO
-            assert [DEMO, NEW] == [
+            assert DEMO and NEW in [
                 Path(combo.itemText(i)).name for i in range(combo.count())
             ]
+
+            # set the combo to new so that after accepting the config wizard should
+            # be visible
+            combo.setCurrentText(NEW if wiz else DEMO)
+
             # simulate click on ok
             init._startup_dialog.accept()
 
+            # only if DEMO cfg was selected, the config wizard should be visible
+            if wiz:
+                assert init._startup_dialog._cfg_wizard.isVisible()
+
         else:
-            assert len(data["paths"]) == 2
             assert not hasattr(init, "_startup_dialog")
             current_cfg = config
             assert str(current_cfg) in data["paths"]
 
-        assert core.systemConfigurationFile() == str(current_cfg)
+        # a config should have been loaded only if DEMO cfg was selected
+        if not wiz:
+            assert core.systemConfigurationFile() == str(current_cfg)
 
     USER_CONFIGS_PATHS.unlink()
