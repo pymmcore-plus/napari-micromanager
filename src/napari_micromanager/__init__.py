cc843abb7ec1782926897a4b406fe9d450821961
Talley Lambert <talley.lambert@gmail.com>
2024-06-29 14:14:48 -0400

    add early import

--- src/napari_micromanager/__init__.py
+++ src/napari_micromanager/__init__.py
@@ -7,6 +7,11 @@ try:
 except PackageNotFoundError:
     __version__ = "uninstalled"
 
+
+# ensure this gets imported before PyQt or other calls to os.add_dll_directory
+# https://github.com/micro-manager/pymmcore/issues/119
+import pymmcore  # noqa: F401
+
 from .main_window import MainWindow
 
 __all__ = ["__version__", "MainWindow"]
