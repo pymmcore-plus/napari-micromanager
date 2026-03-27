f2b35157bf5d203a62a9dceeb916c8dd4252fea9
Talley Lambert <talley.lambert@gmail.com>
2026-03-27 18:06:55 -0400

    fix: update pymmcore-plus and pymmcore-widgets dependencies; use weakref for CMMCorePlus instance (#368)
    * fix: update pymmcore-plus and pymmcore-widgets dependencies; use weakref for CMMCorePlus instance
    
    * fix: update deprecation warnings and refactor get_full_sequence_axes function
    
    * drop pyqt5 pyside2
    
    * fix: update Python version requirements and matrix for CI

--- src/napari_micromanager/_util.py
+++ src/napari_micromanager/_util.py
@@ -16,26 +16,21 @@ except ImportError:
     # key in MDASequence.metadata where we expect to find pymmcore_widgets metadata
     PYMMCW_METADATA_KEY = "pymmcore_widgets"
 
-try:
-    from pymmcore_plus.mda.handlers._util import (
-        get_full_sequence_axes as get_full_sequence_axes,
-    )
-except ImportError:
 
-    def get_full_sequence_axes(sequence: useq.MDASequence) -> tuple[str, ...]:
-        """Get the combined axes from sequence and sub-sequences."""
-        # axes main sequence
-        main_seq_axes = list(sequence.used_axes)
-        if not sequence.stage_positions:
-            return tuple(main_seq_axes)
-        # axes from sub sequences
-        sub_seq_axes: list = []
-        for p in sequence.stage_positions:
-            if p.sequence is not None:
-                sub_seq_axes.extend(
-                    [ax for ax in p.sequence.used_axes if ax not in main_seq_axes]
-                )
-        return tuple(main_seq_axes + sub_seq_axes)
+def get_full_sequence_axes(sequence: useq.MDASequence) -> tuple[str, ...]:
+    """Get the combined axes from sequence and sub-sequences."""
+    # axes main sequence
+    main_seq_axes = list(sequence.used_axes)
+    if not sequence.stage_positions:
+        return tuple(main_seq_axes)
+    # axes from sub sequences
+    sub_seq_axes: list = []
+    for p in sequence.stage_positions:
+        if p.sequence is not None:
+            sub_seq_axes.extend(
+                [ax for ax in p.sequence.used_axes if ax not in main_seq_axes]
+            )
+    return tuple(main_seq_axes + sub_seq_axes)
 
 
 def ensure_unique(path: Path, extension: str = ".tif", ndigits: int = 3) -> Path:
