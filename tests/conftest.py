af904b99d741f3cbac51c852b4af60a322c01029
Talley Lambert <talley.lambert@gmail.com>
2026-03-30 21:25:12 -0400

    ci: speed up tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import itertools
 import logging
 import weakref
 from contextlib import suppress
@@ -10,12 +9,10 @@ from unittest.mock import patch
 
 import napari
 import pytest
-import useq
 from pymmcore_plus import CMMCorePlus
 from pymmcore_plus.experimental.unicore import UniMMCore
 from pymmcore_plus.experimental.unicore.core import _unicore
 
-from napari_micromanager._util import NMM_METADATA_KEY
 from napari_micromanager.main_window import MainWindow
 
 if TYPE_CHECKING:
@@ -38,7 +35,6 @@ _CORE_PARAMS = [
 ]
 
 
-# to create a new CMMCorePlus/UniMMCore for every test
 @pytest.fixture(params=_CORE_PARAMS)
 def core(
     monkeypatch: pytest.MonkeyPatch, request: pytest.FixtureRequest
@@ -66,36 +62,3 @@ def main_window(core: CMMCorePlus, napari_viewer: napari.Viewer) -> MainWindow:
     napari_viewer.window.add_dock_widget(win, name="MainWindow")
     assert core == win._mmc
     return win
-
-
-TIME_PLANS = (None, useq.TIntervalLoops(loops=3, interval=0))
-Z_PLANS = (None, useq.ZRangeAround(range=3, step=0.5))
-CHANNEL_PLANS = (
-    (useq.Channel(config="DAPI", exposure=5),),
-    (useq.Channel(config="DAPI", exposure=5), useq.Channel(config="Cy5", exposure=5)),
-)
-MDAS = [
-    {"time_plan": t, "z_plan": z, "channels": c}
-    for t, z, c in itertools.product(TIME_PLANS, Z_PLANS, CHANNEL_PLANS)
-]
-MDA_IDS = [
-    f"nT={t}-nZ={z}-nC={len(c)}"
-    for t, z, c in itertools.product((0, 3), (0, 7), CHANNEL_PLANS)
-]
-
-
-@pytest.fixture(params=MDAS, ids=MDA_IDS)
-def mda_sequence(request: pytest.FixtureRequest) -> useq.MDASequence:
-    return useq.MDASequence(
-        **request.param, metadata={NMM_METADATA_KEY: {"split_channels": False}}
-    )
-
-
-@pytest.fixture(params=[True, False], ids=["splitC", "no_splitC"])
-def mda_sequence_splits(
-    mda_sequence: useq.MDASequence, request: pytest.FixtureRequest
-) -> useq.MDASequence:
-    if request.param:
-        meta = {"split_channels": True}
-        mda_sequence.metadata[NMM_METADATA_KEY] = meta
-    return mda_sequence
